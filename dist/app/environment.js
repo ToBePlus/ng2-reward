f4f0fa3f81bd09d9ddbe7a07e683b38dde2659f3
ToBePlus <zhangdong@hujiang.com>
2016-06-01 05:47:35 +0800

    bug fixed

---
+++ dist/app/environment.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.environment = {
+    production: false
+};
+//# sourceMappingURL=/Users/worm/Documents/ng2-reward/tmp/broccoli_type_script_compiler-input_base_path-i45Tc3U5.tmp/0/tmp/broccoli_type_script_compiler-input_base_path-i45Tc3U5.tmp/0/src/app/environment.js.map
